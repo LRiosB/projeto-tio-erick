c35619f2ba4ed146ae901bac6392648a8d66deba
LRiosB <lucas.riosb@gmail.com>
2025-07-02 21:13:37 -0300

    one more try

--- generatePDF.py
+++ generatePDF.py
@@ -1,4 +1,5 @@
-from weasyprint import HTML
+from xhtml2pdf import pisa
+import pdfkit
 
 def gerarHTML(input="template.html", output="template.pdf", substitutions:dict[str, str]=dict()):
 
@@ -8,7 +9,31 @@ def gerarHTML(input="template.html", output="template.pdf", substitutions:dict[s
     for target in substitutions:
         htmlContent = htmlContent.replace(target, substitutions[target])
 
-    HTML(string=htmlContent).write_pdf(output)
+    # HTML(string=htmlContent).write_pdf(output)
+
+    saveHTMLTOpdf(htmlContent, output)
+
+def saveHTMLTOpdf(htmlcontent, output):
+    pdfkit.from_string(htmlcontent, output)
+
+def save_html_to_pdf(html_str: str, output_filename: str) -> bool:
+    """
+    Converts HTML string to PDF and saves it as a file.
+    
+    Args:
+        html_str (str): The HTML content to convert.
+        output_filename (str): The name of the output PDF file.
+    
+    Returns:
+        bool: True if successful, False otherwise.
+    """
+    try:
+        with open(output_filename, "wb") as file:
+            result = pisa.CreatePDF(src=html_str, dest=file)
+        return not result.err
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
 
 
 if __name__ == "__main__":
